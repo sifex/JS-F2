85b5314952d6d86eb82cc0b5dd57c65fbf24e46f
Alex <chess2ryme@gmail.com>
2017-04-06 18:55:54 +1000

    Assignment Finished

--- script.js
+++ script.js
@@ -1,128 +1,257 @@
 /**
-* u3162501
-* Personality Quiz
-* JS&F Semester 1 / 2017
-*/
-
-
-// Questions
-var questions = [{
-	ask: "Would you consider yourself social?",
-	answers: [
-		{
-			text: 'Yes',
-			attributes: {
-				happiness: 2,
-				selfish: 0,
-				lonelyness: -1,
-			}
-		},
-		{
-			text: 'No',
-			attributes: {
-				happiness: -1,
-				lonelyness: 1,
-			}
-		}
-	],
-}];
-
-var attributes = {
-	happiness: 0,
-	lonelyness: 0,
-	selfish: 0,
-	perfectionist: 0,
-	optimist: 0
-}
-
-var answers = [{
-
-}]
+ * u3162501
+ * Personality Quiz
+ * JS&F Semester 1 / 2017
+ */
 
 
 /**
-* Box Variables
-*/
+ * Questions
+ * Lists all the questions and their answers,
+ * and the attributes those answers convey
+ * @type {Array}
+ */
+var questions = [{
+        ask: "Would you consider yourself social?",
+        answers: [{
+                text: 'Yes',
+                attributes: {
+                    happiness: 2,
+                    optimist: 1,
+                    lonelyness: -1,
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    happiness: -1,
+                    lonelyness: 1,
+                    optimist: -1
+                }
+            }
+        ],
+    },
+    {
+        ask: "Are you open to new experiences?",
+        answers: [{
+                text: 'Yes',
+                attributes: {
+                    optimist: 2,
+                    happiness: 1,
+                    lonelyness: -1,
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    optimist: -2,
+                    happiness: -1,
+                    lonelyness: 1,
+                }
+            }
+        ],
+    },
+    {
+        ask: "Do unfinished things bother you?",
+        answers: [{
+                text: 'Yes',
+                attributes: {
+                    perfectionist: 5,
+                    optimist: -1
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    perfectionist: -2,
+                    optimist: 1
+                }
+            },
+            {
+                text: 'Sometimes',
+                attributes: {
+                    perfectionist: 2,
+                    happiness: 2,
+                    optimist: 1
+                }
+            },
+        ],
+    },
+    {
+        ask: "Do you see yourself as dependable, self-disciplined?",
+        answers: [{
+                text: 'Yes',
+                attributes: {
+                    perfectionist: 2,
+                    optimist: 2,
+                    happiness: 2,
+                    selfish: 1,
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    perfectionist: -2,
+                    optimist: -1,
+                    happiness: -1,
+                }
+            }
+        ],
+    },
+    {
+        ask: "Do you believe that 42 is the answer to life, the universe and everyting?",
+        answers: [{
+                text: 'Yes',
+                attributes: {
+                    optimist: -2,
+                    happiness: 5
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    optimist: 2,
+                    happiness: 1
+                }
+            },
+            {
+                text: 'What?',
+                attributes: {
+                    selfish: 1,
+                    happiness: -2
+                }
+            }
+        ],
+    },
+    {
+        ask: "Do you watch the Big Bang theory?",
+        answers: [{
+                text: 'No',
+                attributes: {
+                    happiness: 1
+                }
+            },
+            {
+                text: 'No',
+                attributes: {
+                    lonelyness: 1,
+                    happiness: 1
+                }
+            },
+        ],
+    }
+];
+
+
+/**
+ * All possible attributes
+ * These are added up at the end to find out the correct answer
+ * @type {Object}
+ */
+var attributes = {
+    happiness: 0,
+    lonelyness: 0,
+    selfish: 0,
+    perfectionist: 0,
+    optimist: 0
+}
+
+/**
+ * Attribute descriptions
+ * @type {Object}
+ */
+var attributesDescriptions = {
+    happiness: ["happy", "You probably surround youself with other happy people."],
+    lonelyness: ["lonely", "You're probably pretty lonely given your responses."],
+    selfish: ["selfish", "I'm serious, you should probably get a mirror or something."],
+    perfectionist: ["perfectionistic", "You've probably got your thoughts ordered by hue, contrast and alphabetically."],
+    optimist: ["omtimistic", "Life's currently looking pretty good for you eh? Just you wait, life is waiting around that corner with a baseball bat and a bad attitude. Have your towel with you at all times, and DON'T PANIC."]
+}
+
+
+/**
+ * DOM Variables
+ */
 var box;
 var heading;
 var content;
 var answers;
 
-
 /**
-* Question Item
-* Class of each individual question
-* @type {function}
-*/
-var question = new function() {
-	this.content = "";
-	this.options = [{}];
-
-	return this;
-}
-
-/**
-* Quiz Item
-* Quizes contain questions and allows for a more OOP based quiz
-* @type {function}
-*/
+ * Quiz Item
+ * Quizes contain questions and allows for a more OOP based quiz
+ * @type {function}
+ */
 var quiz = new function() {
-	this.questions = questions;
+    this.questions = questions;
+
+    /**
+     * nextQuestion Function
+     * The onClick Handler for each answer
+     * @param  {int} qi    Selected Answer index
+     * @param  {int} index Index of next question
+     */
+    this.nextQuestion = function(qi, index) {
+        /* Save question answer */
+        /* Index Minus One is the current question, as index
+           specifies the next question */
+        this.saveQuestion(qi, index - 1)
+
+        /* Display the next question */
+        this.displayQuestion(index)
+    }
+
+    /**
+     * Display Question Function
+     * @param  {Integer} index
+     */
+    this.displayQuestion = function(index) {
+        /* Display the next question */
+        var questionToDisplay = questions[index];
+        if (questionToDisplay != null) {
+            heading.innerHTML = "Question " + (index + 1);
+            content.innerHTML = questionToDisplay.ask;
+            answers.innerHTML = ""
+            for (var i = 0; i < questionToDisplay.answers.length; i++) {
+                answers.innerHTML += '<button class="answer" onClick="quiz.nextQuestion(' + i + ', ' + (index + 1) + ')">' + questionToDisplay.answers[i].text + '</button>'
+            }
+        } else {
+            var attrSorted = Object.keys(attributes).sort(function(a, b) {
+                return this.attributes[b] - this.attributes[a]
+            })
+
+            var personality = attributesDescriptions[attrSorted[0]][0];
+            var description = attributesDescriptions[attrSorted[0]][1];
+
+            heading.innerHTML = "Answer";
+            content.innerHTML = "After taking this quiz, your look pretty " + personality + ". " + description + " But what do I know, I'm just a quiz";
+            answers.innerHTML = '<button class="reset" onClick="quiz.resetQuiz()">Reset Quiz</button>'
+        }
+    }
 
 	/**
-	 * nextQuestion Function
-	 * The onClick Handler for each answer
-	 * @param  {int} qi    Selected Answer index
-	 * @param  {int} index Index of next question
+	 * Save Question Function
+	 * @param  {integer} qi    Answer Index
+	 * @param  {integer} index Index of Question
+	 * @return {[type]}       [description]
 	 */
-	this.nextQuestion = function(qi, index) {
-		/* Save question answer */
-		/* Index Minus One is the current question, as index
-		   specifies the next question */
-		this.saveQuestion(qi, index - 1)
+    this.saveQuestion = function(qi, index) {
+        /* The question in question */
+        /* Ha, Ha. See what I did there? */
+        var qiq = this.questions[index];
+        var answer = qiq.answers[qi];
+        for (var attr in answer.attributes) {
+            attributes[attr] += answer.attributes[attr];
+        }
+        console.log(attributes)
+    }
 
-		/* Display the next question */
-		this.displayQuestion(index)
-	}
-
-	/**
-	 * Display Question Function
-	 * @param  {Integer} index
-	 */
-	this.displayQuestion = function(index) {
-		/* Display the next question */
-		var questionToDisplay = questions[index];
-		if(questionToDisplay != null) {
-			heading.innerHTML = "Question " + (index + 1);
-			content.innerHTML = questionToDisplay.ask;
-			answers.innerHTML = ""
-			for(var i = 0; i < questionToDisplay.answers.length; i++) {
-				answers.innerHTML += '<button class="answer" onClick="quiz.nextQuestion(' + i + ', ' + (index+1) + ')">' + questionToDisplay.answers[i].text + '</button>'
-			}
-		} else {
-			heading.innerHTML = "Answer";
-			content.innerHTML = "Answer"
-			answers.innerHTML = '<button class="reset" onClick="quiz.resetQuiz()">Reset Quiz</button>'
-		}
-	}
-
-	this.saveQuestion = function(qi, index) {
-		/* The question in question */
-		/* Ha, Ha. See what I did there? */
-		var qiq = this.questions[index];
-		var answer = qiq.answers[qi];
-		for(var attr in answer.attributes) {
-			attributes[attr] += answer.attributes[attr];
-		}
-		console.log(attributes)
-	}
-
-	this.resetQuiz = function() {
-		for(var attr in attributes) {
-			attributes[attr] = 0;
-		}
-		this.displayQuestion(0);
-	}
+    this.resetQuiz = function() {
+        for (var attr in attributes) {
+            attributes[attr] = 0;
+        }
+        this.displayQuestion(0);
+    }
 }
 
 
@@ -130,25 +259,25 @@ var quiz = new function() {
 
 
 /**
-* The initalisation function
-* Starts the Quiz
-* @type {function}
-* @return {[type]} [description]
-*/
+ * The initalisation function
+ * Starts the Quiz
+ * @type {function}
+ * @return {[type]} [description]
+ */
 function init() {
-	// Set Dom Variables
-	querySetDOMElements();
-	quiz.displayQuestion(0);
+    // Set Dom Variables
+    querySetDOMElements();
+    quiz.displayQuestion(0);
 }
 
 
 
 /**
-* Set all variables to their element ID
-*/
+ * Set all variables to their element ID
+ */
 function querySetDOMElements() {
-	box = document.querySelector("#box");
-	heading = box.querySelector("h1");
-	content = box.querySelector("p");
-	answers = box.querySelector(".answers");
+    box = document.querySelector("#box");
+    heading = box.querySelector("h1");
+    content = box.querySelector("p");
+    answers = box.querySelector(".answers");
 }
